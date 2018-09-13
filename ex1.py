2e65e98cbabeaa9bc6aca6efd5e1725c7151b232
Mohamed Moustapha SARR <mm.sarr@live.fr>
2018-09-13 02:21:40 +0000

    Add files via upload
    Synthèse Day 2 : Exercise 1 of 'Python the Hard Way is done'. It was so simple (Basic printing) but it's because i don't have to ignore basic things. I also read some about python.

---
+++ ex1.py
@@ -0,0 +1,10 @@
+#Day 2 12/09/2018 ~ Exercice 1 du livre Python The Hard Way + Un peu de lecture sur Python on edX
+
+#Script very basique en effet!
+print("Hello World!")
+print("Hello again")
+print("I like typing this!")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much not rather you 'not' ")
+print('I "said" do not touch this.')
