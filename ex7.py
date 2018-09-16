522a00f661878639de1cbfef5b21618c9f19ee3b
Mohamed Moustapha SARR <mm.sarr@live.fr>
2018-09-16 21:44:37 +0000

    Exercise 7 and 8
    Printing, Printing.
    
    #PythonTheHardWay

---
+++ ex7.py
@@ -0,0 +1,24 @@
+#Exercise 7, More printing
+print("Sadiya had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Sadiya went.")
+print("."*10)
+
+end1='C'
+end2='h'
+end3='e'
+end4='e'
+end5='s'
+end6='e'
+end7='b'
+end8='u'
+end9='r'
+end10='g'
+end11='e'
+end12='r'
+
+#rassemblons tout ceci
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ') #utilisationdu deuxieme parametre de la fonction print
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+print("End of The Exercise")
