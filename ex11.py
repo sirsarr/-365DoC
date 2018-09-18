9f35dc311be1d1806e8492c63404de2ac7e44cbd
Mohamed Moustapha SARR <mm.sarr@live.fr>
2018-09-18 00:15:39 +0000

    Exercises 9, 10 and 11, PTHW 15 Sept. 18
    More Printing, Triple-Double-Quotes, Triple-Single-Quotes, Input fonction to get a value from the user (via his Keyboard)

---
+++ ex11.py
@@ -0,0 +1,14 @@
+#Exercise10 - Asking Questions PTHW
+
+"""Utilisation de la fonction input() pour receuillir
+une valeur saisie par l'utilisateur à partir du clavier
+"""
+
+print("How old are you?", end =' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
