31fc415ae4b6ee9df434b4df92361ce6d0d008a0
Mohamed Moustapha SARR <mm.sarr@live.fr>
2018-09-19 06:33:55 +0000

    Exercise 12 and 13 - PTHW
    Prompting, parameters, unpacking and variables !

---
+++ ex12.py
@@ -0,0 +1,8 @@
+#Exercise 12 - Prompting people PTHW
+
+age = input("How old are you ? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+#Affichage
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
---
+++ ex13.py
@@ -0,0 +1,9 @@
+#Exercise 13 -  Parameters, Unpacking, Variables
+from sys import argv
+#let the show begin!
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("Your firdt variable is: ", first)
+print("Your second variable is: ", second)
+print("Your third variable is: ", third)
