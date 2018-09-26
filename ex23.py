e73d980174dd81cf3e91eb24d345b53755a374ed
Mohamed Moustapha SARR <mm.sarr@live.fr>
2018-09-26 23:56:30 +0000

    Day 16 Ex23bis -
    Strings, Bytes, and Character Encodings

--- ex23.py
+++ ex23.py
@@ -1,23 +1,25 @@
-print("Let's practice everything.")
-print('You\'d need to know \'bout escapes with \\ that do')
-print('\n newlines and \t tabs.')
-poem = """
-\tThe lovely world
-with logic so firmly planted
-cannot discern \n the needs of love
-nor comprehend passion from intuition
-and requires an explanation
-\n\t\twhere there is none.
-"""
-print("--------------")
-print(poem)
-print("--------------")
+#Ex23 Strings, Bytes and Character Encodings
+import sys
+script, input_encoding, error = sys.argv
 
-five=10-2+3-6
-print(f"This should be five: {five}")
 
-def secret_formula(started):
-    jelly_beans = started * 500
-    jars = jelly_beans / 1000
-    crates = jars / 100
-    return jelly_beans, jars, crates
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file,encoding, errors)
+
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes,"<===>", cooked_string)
+
+
+
+languages = open("languages.txt",encoding="utf-8")
+
+main(languages, input_encoding, error)
